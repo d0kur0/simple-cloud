f2a0079bcd84b214dd82000a2efef5c8e78306bd
d0kur0 <dev.dokuro@gmail.com>
2019-10-02 22:27:30 +0300

    Fix pathes, use variable from build nginx configs section

--- start.sh
+++ start.sh
@@ -12,20 +12,19 @@ fi
 
 source .env
 
-
 export DOCKER_USER="$UID:$GID"
-export SERVER_PATH="$PWD/server"
-export CLIENT_PATH="$PWD/client"
+export SERVER_PATH="$PWD/app/server"
+export CLIENT_PATH="$PWD/app/client"
 export NGINX_CONFIGS="./docker/nginx"
 
-if [ ! -d "docker/nginx/working" ]; then
+if [ ! -d "$NGINX_CONFIGS/working" ]; then
   cp -r "$NGINX_CONFIGS/original" "$NGINX_CONFIGS/working"
 fi
 
 sed -ri "s/server_name[^;]*;/server_name ${SERVER_NAME_CLIENT};/" "$NGINX_CONFIGS/working/client.conf"
 sed -ri "s/server_name[^;]*;/server_name ${SERVER_NAME_SERVER};/" "$NGINX_CONFIGS/working/server.conf"
 
-export VUE_ENV="app/client/.env.local"
+export VUE_ENV="$CLIENT_PATH/.env.local"
 
 if [ -f "$VUE_ENV" ]; then
   rm "$VUE_ENV"
@@ -33,6 +32,11 @@ fi
 
 echo "VUE_APP_SERVER_ADDRESS=$SERVER_NAME_SERVER" >> "$VUE_ENV"
 echo "VUE_APP_CLIENT_ADDRESS=$SERVER_NAME_CLIENT" >> "$VUE_ENV"
+
+docker run --rm --interactive --tty \
+  -u "${UID}:${GID}" \
+  -v "$SERVER_PATH":/app \
+  composer install --ignore-platform-reqs
 
 docker-compose down
 docker-compose build
